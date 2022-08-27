b95689dca15c25f7743cb3e51c8d84a40f0532e8
Nic Laflamme <nic@laflamme.io>
2022-08-27 15:02:44 -0600

    yolo

--- routes/ping.js
+++ routes/ping.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  res.status(204).send();
+  res.status(200).send({ success: true })
 });
 
 router.get('/authenticated', function(req, res) {
-  res.status(204).send();
+  res.status(200).send({ success: true })
 });
 
 module.exports = router;
