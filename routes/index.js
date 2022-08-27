c6bbcd196f506eb0c43146c54582e47cd90c13e8
Nic Laflamme <nic@laflamme.io>
2022-08-27 17:53:58 -0400

    Login user 3 (#12)
    * setup
    
    * removed
    
    * ylo
    
    * yolo
    
    Co-authored-by: natureboylu <natureboyluding@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
+const { constants } = require('buffer');
 var express = require('express');
 var router = express.Router();
+var prisma = require("@prisma/client")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/login', async function(req, res, next ) {
+  const email = req.body.email
+  const password = req.body.password
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email
+    },
+    select: { email: true, 
+    password: true, }
+
+  })
+
+ return res.send( user )
+
 });
 
 module.exports = router;
