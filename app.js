f606f2a15fa02720c6478940385cb79a58bb53f5
Hazbleydy Cortes <90346935+hazac@users.noreply.github.com>
2022-08-27 15:01:32 -0600

    hashing passwords

--- app.js
+++ app.js
@@ -38,4 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, () => console.log('server started'));
+
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+const bcrypt = require('bcryptjs')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/signup', function(req, res, next) {
+  console.log(req.body.password);
+  var pass = req.body.password;
+  const hashPass = saltHash(pass);
+  res.send('hashed', hashPass);
+
+});
+
+function saltHash(pass){
+  console.log('pass in hashPass is ', pass)
+  const saltRounds = 12;
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    console.log('salt', salt);
+    bcrypt.hash(pass, salt, function(err, hash) {
+
+    // returns hash
+    console.log('hash', hash);
+    return hash;
+    });
+  });
+}
 module.exports = router;
+
+
